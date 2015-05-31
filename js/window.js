4d4b0c382cd1d594062e10537ac50f2d80678f97
brandon <budang@utexas.edu>
2015-05-31 23:21:37 +0000

    started implementation of windows overlapping one another, started details of having windows open/closed/etc

--- js/window.js
+++ js/window.js
@@ -1,5 +1,19 @@
+var zindex = 5;
+
+// opened
+var opened = {
+    home: false,
+    internet_window: false,
+    pic_window: false,
+    vid_window: false,
+    game_window: false,
+    folder_window: false
+}
+
 $(document).ready(function() {
     $(".square").click(function() {
+        zindex++;
+        // alert(zindex);
         if($(this).is("#home")) {
         } else if($(this).is("#internet")) {
             openWindow("internet_window");
@@ -15,26 +29,34 @@ $(document).ready(function() {
     });
 });
 
-openWindow = function(divID) {
-    var screenHeight = window.innerHeight;
-    var screenWidth = window.innerWidth;
-    // alert(divID);
-    // var top = Math.floor(Math.random() * (screenHeight * 3 / 4 + 1)) + "px";
-    // var right = Math.floor(Math.random() * ((screenWidth - 75) * 3 / 4 + 1)) + "px";
-    // alert(top + ", " + right);
-    var top = 0;
-    var right = 0;
-    var height = 500 + "px";
-    var width = 700 + "px";
-    
-    // if(divID !== "game_window") {
-    //     // $("canvas").css({"margin": "auto", "position": "absolute"});
-    //     $("#" + divID).css({"position": "absolute", "top": top, "right": right, "height": height, "width": width, "background-color": "Black", "z-index": "1", "box-sizing": "border-box"});
-    // }
-    if(divID === "pic_window") {
-        $("#image_slider").css({"width": "5460px"});
+openWindow = function(winID) {
+    if(opened[winID] === false) {
+        opened[winID] = true;
+        // alert(divID + "/n" + opened[divID]);
+        // var screenHeight = window.innerHeight;
+        // var screenWidth = window.innerWidth;
+        // alert(divID);
+        // var top = Math.floor(Math.random() * (screenHeight * 3 / 4 + 1)) + "px";
+        // var right = Math.floor(Math.random() * ((screenWidth - 75) * 3 / 4 + 1)) + "px";
+        // alert(top + ", " + right);
+        // var top = 0;
+        // var right = 0;
+        // var height = 500 + "px";
+        // var width = 700 + "px";
+        
+        // if(divID !== "game_window") {
+        //     // $("canvas").css({"margin": "auto", "position": "absolute"});
+        //     $("#" + divID).css({"position": "absolute", "top": top, "right": right, "height": height, "width": width, "background-color": "Black", "z-index": "1", "box-sizing": "border-box"});
+        // }
+        if(winID === "pic_window") {
+            $("#image_slider").css({"width": "5460px"});
+        }
+        // alert(zindex);
+        $("#" + winID).css({"display": "block", "position": "relative", "border": "5px double Blue", "z-index": zindex});
+        // var element = document.getElementById(divID),
+        // style = window.getComputedStyle(element),
+        // top = style.getPropertyValue('z-index');
+        // alert(top);
+        // $("#" + divID).css({"background-color": "Black"});
     }
-    
-    $("#" + divID).css({"display": "block", "border": "5px double Blue"});
-    // $("#" + divID).css({"background-color": "Black"});
-}
+};
