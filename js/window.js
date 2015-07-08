1807f186d15b3d3c0c02994b60afcebebbc03c53
brandon <budang@utexas.edu>
2015-07-08 14:20:59 +0000

    removed opened/active objects, used classes instead; icons now change color accordingly depending on state

--- js/window.js
+++ js/window.js
@@ -1,59 +1,47 @@
 var zindex = 0;
 
-// active or opened windows
-var opened = {
-    home: false,
-    internet_window: false,
-    pic_window: false,
-    vid_window: false,
-    game_window: false,
-    folder_window: false
+// square-window mappings
+var relations = {
+    "home": "home",
+    "internet": "internet_window",
+    "pictures": "pic_window",
+    "videos": "vid_window",
+    "game": "game_window",
+    "folder": "folder_window"
 }
-var active = jQuery.extend({}, opened);
+
+function getSquare(windId) {
+    for(var squareId in relations) {
+        if(relations[squareId] === windId) { return squareId; }
+    }
+}
 
 $(document).ready(function() {
     $(".square").click(function() {
-        var wind;
-        zindex++;
-        switch($(this).attr("id")) {
-            case "home":
-                wind = "home";
-                break;
-            case "internet":
-                wind = "internet_window";
-                break;
-            case "pictures":
-                wind = "pic_window";
-                break;
-            case "videos":
-                wind = "vid_window";
-                break;
-            case "game":
-                wind = "game_window";
-                break;
-            case "folder":
-                wind = "folder_window";
-                break;
-         }
-        openWindow(wind);
+        $(this).css("background", "Red");
+        var squareId = $(this).attr("id");
+        openWindow(relations[squareId]);
     });
     
     $(".exit, .min").click(function() {
-        var window = $(this).parent().parent().attr("id");
-        $("#" + window).css("display", "none");
-        active[window] = false;
+        var windId = "#" + $(this).parent().parent().attr("id");
+        $(windId).css("display", "none");
+        $(windId).removeClass("active");
         if($(this).attr("class") === "exit") {
-            opened[window] = false;
+            $(windId).removeClass("opened");
+            var squareId = "#" + getSquare($(windId).attr("id"));
+            $(squareId).css("background", "rgba(255, 255, 255, 0.7)");
         }
     });
 });
 
-openWindow = function(winID) {
-    if(active[winID] === false) {
-        if(opened[winID] === false) {
-            opened[winID] = true;
+openWindow = function(windId) {
+    windId = "#" + windId;
+    if(!($(windId).hasClass("active"))) {
+        $(windId).addClass("active");
+        if(!($(windId).hasClass("opened"))) {
+            $(windId).addClass("opened");
         }
-        active[winID] = true;
         zindex++;
         // alert(divID + "/n" + opened[divID]);
         // var screenHeight = window.innerHeight;
@@ -71,12 +59,12 @@ openWindow = function(winID) {
         //     // $("canvas").css({"margin": "auto", "position": "absolute"});
         //     $("#" + divID).css({"position": "absolute", "top": top, "right": right, "height": height, "width": width, "background-color": "Black", "z-index": "1", "box-sizing": "border-box"});
         // }
-        if(winID === "pic_window") {
+        if(windId === "#pic_window") {
             $("#image_slider").css({"width": "5460px"});
         }
         // alert(zindex);
-        $("#" + winID).css({"display": "inline", "position": "relative", "border": "5px double Blue", "z-index": zindex});
-        $("#" + winID).css({"display": "block"});
+        $(windId).css({"display": "inline", "position": "relative", "border": "5px double Blue", "z-index": zindex});
+        $(windId).css({"display": "block"});
         // var element = document.getElementById(divID),
         // style = window.getComputedStyle(element),
         // top = style.getPropertyValue('z-index');
