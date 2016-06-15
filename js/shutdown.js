d748674471b6261ebb6d3f1385fbd7861da4a95f
Brandon <budang@utexas.edu>
2016-06-14 22:07:51 -0500

    added separate js file for shutdown event handlers; slight delay added after clicking 'quit' button for experience

---
+++ js/shutdown.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    $("#power").click(function() {
+        $("#shutdown").css("display", "block");
+    });
+
+    $("#restart, #quit").mouseenter(function() {
+        $(this).css("background-color", "white");
+        $(this).find(".glyphicon").css("color", "rgba(102, 0, 0, 0.8)");
+    });
+
+    $("#restart, #quit").mouseleave(function() {
+        $(this).css("background-color", "transparent");
+        $(this).find(".glyphicon").css("color", "white");
+    });
+
+    $("#restart").click(function() {
+        window.location.reload();
+    });
+
+    $("#quit").click(function() {
+    	$("*").off("click");
+    	$("*").off("mouseenter");
+    	$("*").off("mouseleave");
+
+        $("#quit, #restart").removeClass("pointer-cursor");
+        $("*").addClass("wait-cursor");
+
+    	setTimeout(function() {
+        	window.close();
+    	}, 1000);
+    });
+
+    $(".exit-shutdown").click(function() {
+		$("#shutdown").css("display", "none"); 
+    });
+});
