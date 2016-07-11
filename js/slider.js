8d52b2c7db60e0f9948953c7eae78dc2dd908e11
Brandon <budang@utexas.edu>
2016-07-10 22:38:48 -0500

    jquerifying and cleaning up code; next and prev buttons now have pointer cursors

--- js/slider.js
+++ js/slider.js
@@ -1,102 +1,95 @@
-var ul;
-var li_items;
-var imageNumber;
-var imageWidth;
-var prev, next;
-var currentPostion = 0;
-var currentImage = 0;
+$(document).ready(function() {
+    var ul, li_items;
+    var imageNumber, imageWidth;
+    var currentPostion = 0, currentImage = 0;
 
-function init() {
-    ul = document.getElementById("image_slider");
-    ul.style.left = 0;
-    li_items = ul.children;
-    imageNumber = li_items.length;
-    imageWidth = li_items[0].children[0].clientWidth;
-    
-    if(imageWidth === 0) {
-        // imageWidth = 420;
-        imageWidth = document.getElementsByClassName("slide_img")[0];
-        imageWidth = window.getComputedStyle(imageWidth);
-        imageWidth = parseInt(imageWidth.getPropertyValue("width"));
-    }
-    ul.style.width = parseInt(imageWidth * imageNumber) + "px";
-    
-    prev = document.getElementById("prev");
-    next = document.getElementById("next");
-    toggleClickOn();
-}
-
-function animate(opts) {
-    var start = new Date();
-    var id = setInterval(function() {
-        var timePassed = new Date() - start;
-        var progress = timePassed / opts.duration;
-        if (progress > 1){
-            progress = 1;
+    // init
+    (function() {
+        ul = $("#image_slider")[0];
+        $(ul).css("left", "0");
+        li_items = $(ul).children();
+        imageNumber = li_items.length;
+        imageWidth = li_items[0].children[0].clientWidth;
+        
+        if(imageWidth === 0) {
+            // imageWidth = 420;
+            imageWidth = $(".slide_img")[0];
+            imageWidth = window.getComputedStyle(imageWidth);
+            imageWidth = parseInt(imageWidth.getPropertyValue("width"));
         }
-        var delta = opts.delta(progress);
-        opts.step(delta);
-        if (progress == 1){
-            clearInterval(id);
-            opts.callback();
-        }
-    }, opts.delay || 17);
-    //return id;
-}
+        $(ul).css("width", parseInt(imageWidth * imageNumber) + "px");
+        
+        toggleClickOn();
+    })();
 
-function slideTo(imageToGo) {
-    var direction;
-    var numOfImageToGo = Math.abs(imageToGo - currentImage);
-    // slide toward left
- 
-    direction = currentImage > imageToGo ? 1 : -1;
-    currentPostion = -1 * currentImage * imageWidth;
-    var opts = {
-        duration:1000,
-        delta:function(p) {
-            return p;
-        },
-        step:function(delta) {
-            ul.style.left = parseInt(currentPostion + direction * delta * imageWidth * numOfImageToGo) + 'px';
-        },
-        callback:function() {
-            currentImage = imageToGo;
-            toggleClickOn();
-        }	
-    };
-    animate(opts);
-}
-
-function onClickPrev() {
-    toggleClickOff();
-    if (currentImage === 0) {
-        slideTo(imageNumber - 1);
-    } else {
-        slideTo(currentImage - 1);
+    function animate(opts) {
+        var start = new Date();
+        var id = setInterval(function() {
+            var timePassed = new Date() - start;
+            var progress = timePassed / opts.duration;
+            
+            if(progress > 1)
+                progress = 1;
+            
+            var delta = opts.delta(progress);
+            opts.step(delta);
+            
+            if(progress === 1){
+                clearInterval(id);
+                opts.callback();
+            }
+        }, opts.delay || 17);
     }
-}
 
-function onClickNext() {
-    toggleClickOff();
-    if (currentImage === imageNumber - 1) {
-        slideTo(0);
-    } else {
-        slideTo(currentImage + 1);
+    function slideTo(imageToGo) {
+        var direction;
+        var numOfImageToGo = Math.abs(imageToGo - currentImage);
+        
+        // slide left
+        direction = currentImage > imageToGo ? 1 : -1;
+        currentPostion = -1 * currentImage * imageWidth;
+        var opts = {
+            duration: 1000,
+            delta: function(p) {
+                return p;
+            },
+            step: function(delta) {
+                $(ul).css("left", parseInt(currentPostion + direction * delta * imageWidth * numOfImageToGo) + 'px');
+            },
+            callback: function() {
+                currentImage = imageToGo;
+                toggleClickOn();
+            }	
+        };
+        animate(opts);
     }
-}
 
-function toggleClickOn() {
-    prev.onclick = function() {
-        onClickPrev();
-    };
-    next.onclick = function() {
-        onClickNext();
-    };
-}
+    function onClickPrev() {
+        toggleClickOff();
+        if(currentImage === 0) 
+            slideTo(imageNumber - 1);
+        else
+            slideTo(currentImage - 1);
+        
+    }
 
-function toggleClickOff() {
-    prev.onclick = function() {};
-    next.onclick = function() {};
-}
+    function onClickNext() {
+        toggleClickOff();
+        if(currentImage === imageNumber - 1) 
+            slideTo(0);
+        else
+            slideTo(currentImage + 1);
+        
+    }
 
-window.onload = init;
+    function toggleClickOn() {
+        $("#prev, #next").on("click");
+
+        $("#prev").click(onClickPrev);
+        $("#next").click(onClickNext);
+    }
+
+    function toggleClickOff() {
+        $("#prev, #next").off("click");
+    }
+});
