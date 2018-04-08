78bd950a0ce6f1a9b2b8c1ad8a09e497f755d843
Brandon Dang <frogninja@sbcglobal.net>
2018-04-08 11:03:21 -0500

    adding a button specifically for the resume

--- me/js/window.js
+++ me/js/window.js
@@ -1,34 +1,42 @@
 var zindex = 0;
 var screenHeight = window.innerHeight;
 var screenWidth = window.innerWidth;
-var maxHeight = Math.floor(screenHeight / 3), minHeight = Math.floor(screenHeight / 5);
-var maxWidth = Math.floor(screenWidth / 3), minWidth = Math.floor(screenWidth / 5);
+var maxHeight = Math.floor(screenHeight / 3),
+    minHeight = Math.floor(screenHeight / 5);
+var maxWidth = Math.floor(screenWidth / 3),
+    minWidth = Math.floor(screenWidth / 5);
 
 $(document).ready(function() {
     $(".window").addClass("new");
-    
+
     $(".resize-drag").draggable({
         drag: function(event) { bringForward(this); },
         handle: ".topbar"
     });
-    
+
     /*for(var squareId in relations) {
         var windowId = "#" + squareId + "_window";
         $(windowId).resizable({
             resize: function(event) {
-                
+
             },
             minHeight: '',
             minWidth: ''
         })
     }*/
-    
+
     $(".square").click(function() {
         $(this).css("background-color", "rgba(255, 255, 255, 0.7)");
         var squareId = $(this).attr("id");
+
+        if(squareId == "cv") {
+            window.open("./assets/resume.pdf", "_blank");
+            return;
+        }
+
         openWindow(squareId + "_window");
     });
-    
+
     $(".exit, .min").click(function() {
         var windowId = "#" + $(this).parent().parent().attr("id");
         hideWindow(windowId);
@@ -42,7 +50,7 @@ $(document).ready(function() {
             $(squareId).css("background-color", "rgba(192, 192, 192, 0.5)");
         }
     });
-    
+
     $(".window").click(function() {
         bringForward(this);
     });
@@ -51,7 +59,7 @@ $(document).ready(function() {
 function openWindow(windowId) {
     windowId = "#" + windowId;
     var css = { "position": "absolute", "z-index": ++zindex };
-    
+
     if(!($(windowId).hasClass("opened"))) {
         // open new window (with delay for flare)
         $("*").css("cursor", "progress");
@@ -61,18 +69,17 @@ function openWindow(windowId) {
 
             $(windowId).addClass("opened");
             $(windowId).addClass("active");
-            
+
             if(windowId === "#pictures_window")
                 $("#image_slider").css({"width": "5460px"});
-            
-            
-            // randomize position only when opened the first time    
+
+            // randomize position only when opened the first time
             if($(windowId).hasClass("new")) {
                 css.top = Math.floor(Math.random() * (screen.height / 3 - minHeight + 1)) + minHeight / 2 + "px";
                 css.left = Math.floor(Math.random() * (screen.width / 2 - minWidth + 1)) + minWidth / 2 + "px";
                 $(windowId).removeClass("new");
             }
-        
+
             $(windowId).css(css);
             $("*").css("cursor", "default");
             $("#status span, p.file, img.file, a, .square").css("cursor", "pointer");
@@ -80,7 +87,7 @@ function openWindow(windowId) {
     } else if(!$(windowId).hasClass("active")) {
         // reactivate minimized window
         $(windowId).fadeIn("fast");
-        
+
         $(windowId).addClass("active");
         $(windowId).css(css);
     } else {
